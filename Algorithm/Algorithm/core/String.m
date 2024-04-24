574c59aa0290cb7c880bb2387c118e2105e26950
mapeng <mapeng712@gmail.com>
2024-04-24 08:53:44 +0800

    lengthOfLongestSubstring

--- Algorithm/Algorithm/core/String.m
+++ Algorithm/Algorithm/core/String.m
@@ -16,10 +16,12 @@
 
 + (void)reverseWord
 {
-    NSString *str = @" are  you ok ";
-    NSLog(@"%@",[self reverseWords:str]);
+//    NSString *originalString = @"  Hello,     World! How are you?   ";
+    NSString *inputString = @"abcabcbb";
+    NSLog(@"%ld",(long)[self lengthOfLongestSubstring:inputString]);
 }
 
+// 151 反转单词
 + (NSString *)reverseWords:(NSString *)s {
     NSCharacterSet *charSet = [NSCharacterSet whitespaceAndNewlineCharacterSet];
     NSArray<NSString *> *words = [s componentsSeparatedByCharactersInSet:charSet];
@@ -34,6 +36,29 @@
     
     // 将反转后的单词数组转换为单个字符串
     return [filteredWords componentsJoinedByString:@" "];
+}
+
+// 第 3 题，无重复字符的最长子串  滑动窗口(Sliding Window)方法
++ (NSInteger)lengthOfLongestSubstring:(NSString *)s {
+    NSInteger n = [s length];
+    NSInteger ans = 0;
+    NSMutableDictionary<NSString *, NSNumber *> *indexMap = [NSMutableDictionary dictionary];
+
+    for (NSInteger j = 0, i = 0; j < n; j++) {
+        NSString *currentChar = [s substringWithRange:NSMakeRange(j, 1)];
+        
+        if ([indexMap objectForKey:currentChar] != nil) {
+            // 更新左指针 i
+            i = MAX([[indexMap objectForKey:currentChar] integerValue] + 1, i);
+        }
+        
+        // 更新最长子串的长度
+        ans = MAX(ans, j - i + 1);
+        // 存储或更新字符的索引
+        [indexMap setObject:@(j) forKey:currentChar];
+    }
+    
+    return ans;
 }
 
 @end
