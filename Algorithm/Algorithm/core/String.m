9f92cc864d510ab272e1991c65f8dc6fbb4368ea
mapeng <mapeng712@gmail.com>
2024-04-26 08:31:00 +0800

    159

--- Algorithm/Algorithm/core/String.m
+++ Algorithm/Algorithm/core/String.m
@@ -17,8 +17,8 @@
 + (void)reverseWord
 {
 //    NSString *originalString = @"  Hello,     World! How are you?   ";
-    NSString *inputString = @"abcabcbb";
-    NSLog(@"%ld",(long)[self lengthOfLongestSubstring:inputString]);
+    NSString *inputString = @"ccaabbcb";//@"abcabcbb";
+    NSLog(@"%ld",(long)[self lengthOfLongestSubstringTwoDistinct:inputString]);
 }
 
 // 151 反转单词
@@ -39,6 +39,10 @@
 }
 
 // 第 3 题，无重复字符的最长子串  滑动窗口(Sliding Window)方法
+// 注：找出每一个字符作为最后一个字符的无重复子串的长度，就知道结果了。
+// 假定目前最大长度 i到j， j 作为最后一个字符，然后再把 j++的元素放进去，
+// 如果 j++位置的元素前面存在， 则和 i 比较下，如果在 i 和 j++之间，则更新下 i
+// j++结尾的长度则为 newi 到 j++
 + (NSInteger)lengthOfLongestSubstring:(NSString *)s {
     NSInteger n = [s length];
     NSInteger ans = 0;
@@ -59,6 +63,24 @@
     }
     
     return ans;
+}
+
++ (NSInteger)lengthOfLongestSubstringTwoDistinct:(NSString *)s {
+    NSMutableDictionary *map = [NSMutableDictionary dictionaryWithCapacity:2];
+    NSInteger start = 0, maxLength = 0;
+    for (NSInteger end = 0; end < s.length; end++) {
+        map[[s substringWithRange:NSMakeRange(end, 1)]] = @(end);
+        if (map.count > 2) {
+            NSInteger delIndex = NSIntegerMax;
+            for (NSNumber *value in [map allValues]) {
+                delIndex = MIN(delIndex, value.integerValue);
+            }
+            [map removeObjectForKey:[s substringWithRange:NSMakeRange(delIndex, 1)]];
+            start = delIndex + 1;
+        }
+        maxLength = MAX(maxLength, end - start + 1);
+    }
+    return maxLength;
 }
 
 @end
